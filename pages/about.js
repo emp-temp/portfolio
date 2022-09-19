4a9c988b3ab8bd58bb4dff849afab75aa5b9c0a8
empty <103437238+emp-temp@users.noreply.github.com>
2022-09-19 10:49:33 +0900

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -4,8 +4,8 @@ import Hero from 'components/hero';
 
 import Image from 'next/image'
 
-// import temple from 'images/temple.jpg'
-import eyecatch from 'images/about.jpg'
+import temple from 'images/temple.jpg'
+// import eyecatch from 'images/about.jpg'
 import imgstyles from 'styles/img.module.css'
 
 
@@ -21,7 +21,7 @@ export default function About() {
 
       <figure>
         <Image
-          src={eyecatch}
+          src={template}
           alt=""
           layout="responsive"
           sizes="(min-width: 1152px) 1152px, 100vw"
